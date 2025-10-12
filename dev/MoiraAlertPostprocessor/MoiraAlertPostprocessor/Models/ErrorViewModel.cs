86ebc09690123d2c2aae2240a751a9807cd35652
OK <143250301+OK219@users.noreply.github.com>
2025-10-12 23:27:58 +0500

    empty proj

---
+++ dev/MoiraAlertPostprocessor/MoiraAlertPostprocessor/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MoiraAlertPostprocessor.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
